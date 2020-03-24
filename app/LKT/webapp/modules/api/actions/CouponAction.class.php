f54a391dfcf918ebdb36f91bff5f23e4e54b5c05
疯子技师 <yuhuidog@163.com>
2020-03-24 14:16:59 +0800

    优化：后台部分功能

--- app/LKT/webapp/modules/api/actions/CouponAction.class.php
+++ app/LKT/webapp/modules/api/actions/CouponAction.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
-
+my_coupon
  * [Laike System] Copyright (c) 2017-2020 laiketui.com
 
  * Laike is not a free software, it under the license terms, visited http://www.laiketui.com/ for more details.
