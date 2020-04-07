1ad1785566bb24915fc64184ea3872292d857fff
疯子技师 <yuhuidog@163.com>
2020-04-07 12:09:58 +0800

    修复：小程序积分问题

--- miniprogram/pages/user/score.js
+++ miniprogram/pages/user/score.js
@@ -80,7 +80,7 @@ Page({
   getRequest: function () {
      var that = this
      wx.request({
-       url: app.d.ceshiUrl + '&action=sign&m=integral',
+       url: app.d.ceshiUrl + '&index.php?module=api&action=pi&p=sign&c=Home&m=integral',
        method: 'post',
        data: {
          openid: app.globalData.userInfo.openid,
