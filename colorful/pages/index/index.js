5c1eb2d8f365e7f393faadccfe14e24a6be2c7ba
疯子技师 <yuhuidog@163.com>
2020-05-07 07:19:39 +0800

    修复：color皮肤下拉事件

--- colorful/pages/index/index.js
+++ colorful/pages/index/index.js
@@ -33,10 +33,8 @@ Page({
   },
   //下拉事件
   onPullDownRefresh: function () {
-    wx.showNavigationBarLoading() //在标题栏中显示加载
-    this.onLoad();
-    wx.hideNavigationBarLoading() //完成停止加载
-    wx.stopPullDownRefresh() //停止下拉刷新
+    var that = this;
+    that.getMore();
   },
   current1Change: function (e) {
     this.setData({
