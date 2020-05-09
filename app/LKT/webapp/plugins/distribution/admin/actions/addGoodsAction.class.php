07e0b22137d9f35eaad6a9f30d04b197101b5e16
疯子技师 <yuhuidog@163.com>
2020-05-09 14:54:49 +0800

    新增：分销商品显示

--- app/LKT/webapp/plugins/distribution/admin/actions/addGoodsAction.class.php
+++ app/LKT/webapp/plugins/distribution/admin/actions/addGoodsAction.class.php
@@ -11,18 +11,111 @@ class addGoodsAction extends PluginAction
 
     public function getDefaultView()
     {
-        $request = $this->getContext()->getRequest();
-        $m = $request->getParameter('m');
-        if ($m != '') {
-            $this->$m();
-            exit;
-        }
+
     }
 
     //查询商品
     public function pro_query()
     {
+        $db = DBAction::getInstance();
+        $request = $this->getContext()->getRequest();
+        $my_class = addslashes(trim($request->getParameter('my_class')));
+        $my_brand = addslashes(trim($request->getParameter('my_brand')));
+        $pro_name = addslashes(trim($request->getParameter('pro_name')));
 
+        $brand = "";
+        $pagesize = $request->getParameter('pagesize');
+        $pagesize = $pagesize ? $pagesize : 10;
+        // 每页显示多少条数据
+        $page = $request->getParameter('page');
+
+        if ($page) {
+            $start = ($page - 1) * $pagesize;
+        } else {
+            $start = 0;
+        }
+        $data = array();
+        if ($my_class) {
+            $data['product_class'] = "%$my_class%";
+        }
+        if ($my_brand) {
+            $data['brand_id'] = '$my_brand';
+        }
+        if ($pro_name) {
+            $data['product_title'] = "%$pro_name%";
+        }
+
+        //所有商品分类
+        $sql01 = "select cid,pname from lkt_product_class where sid = 0 and recycle = 0";
+        $rr = $db->select($sql01);
+        $product_class = '';
+        foreach ($rr as $key => $value) {
+            $c = '-' . $value->cid . '-';
+            //判断所属类别 添加默认标签
+            if ($my_class == $c) {
+                $product_class .= '<option selected="selected" value="' . $c . '">' . $value->pname . '</option>';
+            } else {
+                $product_class .= '<option  value="' . $c . '">' . $value->pname . '</option>';
+            }
+            //循环第一层
+            $sql_e = "select cid,pname from lkt_product_class where sid = $value->cid and recycle = 0 ";
+            $r_e = $db->select($sql_e);
+            if ($r_e) {
+                $hx = '-----';
+                foreach ($r_e as $ke => $ve) {
+                    $cone = $c . $ve->cid . '-';
+                    //判断所属类别 添加默认标签
+                    if ($my_class == $cone) {
+                        $product_class .= '<option selected="selected" value="' . $cone . '">' . $hx . $ve->pname . '</option>';
+                    } else {
+                        $product_class .= '<option  value="' . $cone . '">' . $hx . $ve->pname . '</option>';
+                    }
+                    //循环第二层
+                    $sql_t = "select cid,pname from lkt_product_class where sid = $ve->cid and recycle = 0";
+                    $r_t = $db->select($sql_t);
+                    if ($r_t) {
+                        $hxe = $hx . '-----';
+                        foreach ($r_t as $k => $v) {
+                            $ctow = $cone . $v->cid . '-';
+                            //判断所属类别 添加默认标签
+                            if ($my_class == $ctow) {
+                                $product_class .= '<option selected="selected" value="' . $ctow . '">' . $hxe . $v->pname . '</option>';
+                            } else {
+                                $product_class .= '<option  value="' . $ctow . '">' . $hxe . $v->pname . '</option>';
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        //全部商品
+        $sql01 = "select min(b.num) as num,min(a.attribute) attribute,min(a.price) price,min(a.id) AS attr_id,min(b.id) id,min(b.product_title) product_title,min(b.imgurl) imgurl
+                from lkt_configure as a 
+                left join lkt_product_list as b on a.pid = b.id 
+                where b.recycle = 0 and b.num >0 and a.num > 0 and b.status = 0 group by a.pid ";
+        $res = $db->select($sql01);
+
+        $appConfig = $this->getAppInfo();
+        $img = $appConfig['imageRootUrl'];
+
+        foreach ($res as $k => $v) {
+
+            $attr = unserialize($v->attribute);
+            $attr = array_values($attr);
+            if ($attr) {
+                    if (gettype($attr[0]) != 'string') unset($attr[0]);
+            }
+            $res[$k]->image = $img . $v->imgurl;
+            $res[$k]->attr =  implode(',', $attr);
+
+
+        }
+        $total = count($res);
+        $res = array_slice($res, $start, $pagesize);
+        echo json_encode(array('res' => $res, 'product_class' => $product_class, 'brand' => $brand, 'total' => $total, 'page' => $page));
+        exit;
     }
 
     public function execute()
--- app/LKT/webapp/plugins/distribution/admin/actions/goodsAction.class.php
+++ app/LKT/webapp/plugins/distribution/admin/actions/goodsAction.class.php
@@ -11,57 +11,7 @@ class goodsAction extends PluginAction
 
     public function getDefaultView()
     {
-        $name = $request->getParameter("username"); // 商品名称
-        $pagesize = $request->getParameter('pagesize');
-        $status = $request->getParameter('status') ? $request->getParameter('status') : 2;
-        $pagesize = $pagesize ? $pagesize : 10;
-
-        // 每页显示多少条数据
-        $page = $request->getParameter('page');
-        if ($page) {
-            $start = ($page - 1) * $pagesize;
-        } else {
-            $start = 0;
-        }
-        $data = array();
-        //如果名字不为空
-        if ($name != '') {
-            $data['name'] = "%$name%";
-        }
-
-        $appConfig = $this->getAppInfo();
-        $img = $appConfig['imageRootUrl'];
-
-        $res = array();
-        $res1 = array();
-        $res = DistributionModel::selectProAll($data); //查询所有分销商品
-
-        $total = count($res);
-        if ($res) {
-            $res1 = array_slice($res, $start, $pagesize); //分页
-            foreach ($res1 as $key => $value) {
-                $pid = $value->pro_id;
-                $data = array();
-                $sel = "min(price) as price";
-                $data['pid'] = $pid;
-                $r_s = Configure::selectall($sel, $data); //查询商品对应的商品规格
-                if ($r_s) {
-                    $res1[$key]->setCustomerAttr("price",  $r_s[0]->price);
-                } else {
-                    $res1[$key]->setCustomerAttr("price",  '0');
-                }
-                $res1[$key] = $res1[$key]->getAllAttrs();
-                $res1[$key]->imgurl = $img . $res1[$key]->imgurl;
-            }
-        }
-
-        $pager = new ShowPager($total, $pagesize, $page);
-        $url = "index.php?module=pi&p=distribution&c=goods&name=$name&pagesize=" . urlencode($pagesize);
-        $pages_show = $pager->multipage($url, $total, $page, $pagesize, $start, $para = '');
-        $request->setAttribute("list", $res1);
-        $request->setAttribute("name", $name);
-        $request->setAttribute("status", $status);
-        $request->setAttribute("pages_show", $pages_show);
+        
         return View::INPUT;
     }
 
