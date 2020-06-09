bb256b0604292606dfb7ba467d91fe9de57d53c4
疯子技师 <yuhuidog@163.com>
2020-06-09 10:04:55 +0800

    新增：我的团队接口

--- app/LKT/webapp/plugins/distribution/front/distributionAction.class.php
+++ app/LKT/webapp/plugins/distribution/front/distributionAction.class.php
@@ -73,8 +73,9 @@ class distributionAction extends PluginAction
     }
 
 
+    //确认收货后增加佣金明细
     public function detailed_commission()
-    {//确认收货后增加佣金明细
+    {
         $db = DBAction::getInstance();
         $request = $this->getContext()->getRequest();
         $order_id = $request->getParameter('order_id'); // 订单号
@@ -85,13 +86,15 @@ class distributionAction extends PluginAction
 
     }
 
+    //返现
     public function commission()
-    {//返现
+    {
 
     }
 
+    //会员人数
     public function membership()
-    {//会员人数
+    {
         $db = DBAction::getInstance();
         $request = $this->getContext()->getRequest();
         $pagesize = 10;
@@ -102,37 +105,26 @@ class distributionAction extends PluginAction
         }
         $openid = addslashes($request->getParameter('openid'));
         $r = $db->select("select user_id from lkt_user where wx_id = '$openid' ");
-        $num = 0;
-        $total = 0;
-        $r01 = '';
         if ($r) {
             $user_id = $r[0]->user_id;
-            $r01 = $db->select("select user_id,user_name,headimgurl,wx_id as openid,Register_data from lkt_user where Referee = '$user_id' order by Register_data desc limit $start,$pagesize");
-            $r001 = $db->select("select user_id,user_name,headimgurl,wx_id as openid,Register_data from lkt_user where Referee = '$user_id' order by Register_data desc");
-            $num = count($r001);
-            $total = ceil($num / $pagesize);
-            if ($r01) {
-                foreach ($r01 as $key => $value) {
-                    $user_id01 = $value->user_id;
-                    $r02 = $db->selectrow("select user_id from lkt_user where Referee = '$user_id01'");
-                    $value->num = $r02;
-                }
-            }
+            $sql = "select a.*,b.wx_name parent_name from lkt_user AS a left join lkt_user AS b ON a.Referee = b.user_id where a.Referee='$user_id'  order by a.id desc  ";
+            $r01 = $db->select($sql);
+            echo json_encode(array('r01' => $r01));
         }
-
-        echo json_encode(array('r01' => $r01, 'num' => $num, 'total' => $total));
         exit();
 
     }
 
+    //预计佣金
     public function money()
-    {//预计佣金
+    {
 
 
     }
 
+    //佣金详情
     public function show()
-    {//佣金详情
+    {
 
     }
 }
