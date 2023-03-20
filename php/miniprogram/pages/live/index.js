812e1525cd425ae42bf36d8ecdc8a03143b3f599
bettershop <yuhuidog@163.com>
2023-03-20 09:01:16 +0800

    直播

--- php/miniprogram/pages/live/index.js
+++ php/miniprogram/pages/live/index.js
@@ -6,7 +6,7 @@ Page({
   },
 
   onLoad: function (options) {
-    let roomId = 3 // 房间号
+    let roomId = 1 // 房间号
     let customParams = {}
     wx.navigateTo({
       url: `plugin-private://wx2b03c6e691cd7370/pages/live-player-plugin?room_id=${roomId}&custom_params=${encodeURIComponent(JSON.stringify(customParams))}`
